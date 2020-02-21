55feab5965a6ad89b97d714a3c6de3adf5d660e7
Yurika Namba <yurikanamba@gmail.com>
2020-02-21 19:49:40 +0900

    working on stringifyJSON when argument is an object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,29 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (value) => {
+  const singleQuote = "'";
+
+  if (typeof value === "string") {
+    return `"${value}"`;
+  }
+
+  //if it's an array
+  if (Array.isArray(value)) {
+    const stringifiedArray = [];
+    value.forEach((value) => {
+      stringifiedArray.push(stringifyJSON(value));
+    });
+    return `[${stringifiedArray}]`;
+  }
+
+  //if it's an object
+  if (Object.prototype.toString.call(value) === "[object Object]") {
+    const stringifiedObject = "";
+    for (var key in value) {
+      return `{${stringifyJSON(key)}:${stringifyJSON(value[key])}}`;
+    }
+    //return stringifiedObject;
+  }
+
+  return "" + value;
 };
