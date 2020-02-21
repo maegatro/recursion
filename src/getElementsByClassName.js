a4881483dbf5e207ca068789c6da165650bf351d
Yurika Namba <yurikanamba@gmail.com>
2020-02-21 18:43:35 +0900

    completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,19 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  let elements = [];
+
+  function findElements(node) {
+    //base
+    if (node.classList && node.classList.contains(className)) {
+      elements.push(node);
+    }
+    //recursive
+    node.childNodes.forEach((element) => {
+      findElements(element);
+    });
+  }
+
+  findElements(document.body);
+  return elements;
 };
