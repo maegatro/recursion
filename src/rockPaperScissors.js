4d4e6e17d7740d434fa4a8fe72c75ca7e075cb9c
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-02-24 16:27:54 +0900

    initial commit

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,20 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (n=3) => {
+  const results = [];
+  const weapons = ['rock', 'paper', 'scissors'];
+  
+  function recurse(roundsLeft, played) {
+    if(roundsLeft === 0) {
+      results.push(played);
+      return;
+    }
+    for(let i = 0; i < weapons.length; i++) {
+      const current = weapons[i];
+      recurse(roundsLeft-1, played.concat(current));
+    }
+  };
+  recurse(n, []);
+  return results;
 };
+
