ed1ae52f6b81f809f2d352c32ff6f768e1f66868
pollyj <55009539+pollyj@users.noreply.github.com>
2020-02-24 21:26:13 +0900

    wrote stringifyJSON code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,53 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (value) => {
+  let array = [];
+  let object = [];
+
+  //primitives
+  if (
+    typeof value === "number" || 
+    typeof value === "boolean" || 
+    value === null) {
+      return "" + value;
+  } else if (typeof value === "string") {
+    return "\"" + value + "\"";
+  }
+
+  //arrays
+  else if (Array.isArray(value)) {
+    if (value.length === 0) {
+      return "[]";
+    } else {
+      for (const element of value) {
+        array.push(stringifyJSON(element));
+      }
+      return "[" + array + "]";
+    }
+  }
+
+  //objects
+  else if (typeof value === "object") {
+
+    if (Object.keys(value).length === 0) {
+      return "{}";
+    } else {
+      for (const key in value) {
+        let quotedKey = "\"" + key + "\":";
+        let quotedKeyVal = "\"" + value[key] + "\"";
+
+        if (typeof value[key] === "string") {
+          object.push(quotedKey + quotedKeyVal);
+        } else if (
+          typeof value[key] === "boolean" ||
+          typeof value[key] === "number" ||
+          value[key] === null) {
+          object.push(quotedKey + value[key]);
+        } else if (typeof value[key] === "object") {
+          object.push(quotedKey + stringifyJSON(value[key]));
+        }
+      }
+      return "{" + object + "}"
+    }
+  }
 };
