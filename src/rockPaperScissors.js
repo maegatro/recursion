5624583a312359cf631b40d0d38a4de6b316b644
Mako Kusuda <mako.kusuda@gmail.com>
2020-02-25 08:00:27 +0900

    Wrote rockPaperScissors

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,20 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (round = 3) => {
+  const weapons = ["rock", "paper","scissors"];
+  let out = [];
+
+  const play = (round, result = []) => {
+    if (round < 1){
+      out.push(result);
+      return;
+    }else {
+      for (let i = 0; i < weapons.length; i++){
+        const weapon = weapons[i];
+        play((round - 1), result.concat(weapon));
+      }
+    }
+  }
+  play(round);
+  return out;
 };
