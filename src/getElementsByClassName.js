fc63db4f07938a12696749c143ac4cebd0d17827
Eri Shimada <shimaeri515@gmail.com>
2020-02-25 19:52:18 +0900

    completed getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,9 +3,22 @@
 const getElementsByClassName = (className) => {
   // YOUR CODE HERE
   let result = [];
-  const traverseDom = (element)=>{
-    element.className.filter()
+  let body = document.body;//a container
+  const findClass = (element)=>{
+    if(element.classList && element.classList.contains(className)){
+      result.push(element)
+    }
   }
+  function testNodes(node,test){
+    test(node);
+    node = node.firstChild;
+    while(node){
+      testNodes(node,test);
+      node = node.nextSibling
+    }
+  }
+  testNodes(body,findClass)
   return result;
 };
-getElementsByClassName()
+
+
