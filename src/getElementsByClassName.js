cede7a030faecc9df48bb03c175beb8715ebebe3
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-26 08:53:13 +0900

    getElementsByClassName Passes tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,34 +1,28 @@
 /* exported getElementsByClassName */
-
-// The contains() method returns a Boolean value indicating whether a node is a descendant of a specified node.
-// A descendant can be a child, grandchild, great-grandchild, and so on.
-
+// The contains() method returns a Boolean value indicating whether a node is a descendant of a specified node.A descendant can be a child, grandchild, great-grandchild, and so on.
 
 const getElementsByClassName = (className,node) => {
   // YOUR CODE HERE
   let results = [];
-
+  
   if(!node){
     node = document.body;
   } else {
     node = node;
   }
 
-  if (node.classList && node.classList.contains(className)) {
+  if (node.classList && node.classList.contains(className)){
     results.push(node);
+    console.log(node);
   }
-
+  
   let children = node.childNodes;
-
   if (children) {
-
-    for (let i = 0; i < children.length; i++) {
-
-      let child = children[i];
-
+    for(let child of children){
       results = results.concat(getElementsByClassName(className, child));
+      console.log(results)
     }
   }
-
   return results;
 };
+console.log(getElementsByClassName("testing",document.body))
