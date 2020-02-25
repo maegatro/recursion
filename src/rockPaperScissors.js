7d9971c673fcbc1c93b8c566d4692feba35b3391
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-25 15:38:15 +0900

    Rock Paper Scissors works

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,7 +1,26 @@
-  
 /* exported rockPaperScissors */
-
-const rockPaperScissors = () => {
+//debugger;
+const rockPaperScissors = (n=3) => {
   // Your code here
+let options = ['rock','paper','scissors']
+let hands = [];
+let current = ''
 
+  function play(n,result=[]){
+    if (n === 0){
+      //something that ends recursion
+      hands.push(result)
+      return;
+    } else {
+      for(let i = 0; i< options.length; i++){
+        current = options[i];
+        play(n - 1, result.concat(current));
+      }
+    }
+  }
+  play(n);
+  return hands;
 };
+
+//console.log(rockPaperScissors(3));
+//console.log(rockPaperScissors(4));
