23a152dce2d531c1fe177aeed630761f18e86e7a
Yurika Namba <yurikanamba@gmail.com>
2020-02-25 09:48:19 +0900

    completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,16 @@
 /* exported stringifyJSON */
 
 const stringifyJSON = (value) => {
-  const singleQuote = "'";
+  //if it's a number, boolean, null
+  if (
+    typeof value === "number" ||
+    typeof value === "boolean" ||
+    value === null
+  ) {
+    return "" + value;
+  }
 
+  //if it's a string
   if (typeof value === "string") {
     return `"${value}"`;
   }
@@ -18,12 +26,15 @@ const stringifyJSON = (value) => {
 
   //if it's an object
   if (Object.prototype.toString.call(value) === "[object Object]") {
-    const stringifiedObject = "";
-    for (var key in value) {
-      return `{${stringifyJSON(key)}:${stringifyJSON(value[key])}}`;
-    }
-    //return stringifiedObject;
-  }
+    const stringifiedObject = [];
 
-  return "" + value;
+    for (var key in value) {
+      if (stringifyJSON(value[key]) !== undefined)
+        stringifiedObject.push(
+          stringifyJSON(key) + ":" + stringifyJSON(value[key])
+        );
+    }
+
+    return "{" + stringifiedObject + "}";
+  }
 };
