20285f6d2b099363ad9e7fc2f7b0db298b3c9d10
Saggitarie <saggitarie@gmail.com>
2020-02-26 15:32:05 +0900

    Articulate my thoughts and understanding of the articles on Recursion into comments

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,13 @@
 /* exported rockPaperScissors */
 
 const rockPaperScissors = () => {
-  // Going to work on this first
+  // Declare variable for final output and weapons
+
+  // Write Base Case here
+    // When number of rounds is less than 0
+
+  // Write Recusive Case here
+    // 4 round for all three patterns: rock, paper and scissors
+
+  return;
 };
