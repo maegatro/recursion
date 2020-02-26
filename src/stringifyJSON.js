29299d3a855ff460837c5c407443ea2f17f44777
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-26 19:42:04 +0900

    fixed linter errors

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,43 +1,6 @@
-// const stringifiableValues = [
-//     100000,
-//     true,
-//     false,
-//     null,
-//     "Code Chrysalis",
-//     [],
-//     [10000],
-//     ["yan"],
-//     [10000, "kani"],
-//     [1, 2, 1000, 0, -1, -3, -3.14, 3.14, 0.1099999, 1000.9999],
-//     [[[["rachel"]]]],
-//     [10000, [[1, 2, 3, 4], 5, 8]],
-//     {},
-//     { a: "is for apple" },
-//     { foo: true, bar: false, baz: null },
-//     { "boolean, true": true, "boolean, false": false, null: null,perro:["yo",1,true] },
-//     // basic nesting
-//     { a: { b: "c" } },
-//     { a: ["b", "c"] },
-//     [{ a: "b" }, { c: "d" }],
-//     { a: [], c: {}, b: true }
-//   ];
-
-
-// let testString = {
-//     Name:"Gatitas",
-//     aFunction: () => {console.log("this is a function")},
-//     Cat:"Dog",
-//     undefined:undefined,
-//     Dog:"Cat",
-//     null:null,
-//     Pet:"Cool"
-// };
-
-
 //debugger;
 function stringifyJSON(input) {
     // YOUR CODE HERE
-    
     /// test for primitive types
     //console.log("the type of input is", typeof(input))
     if (typeof(input) === 'number' || typeof(input) === 'boolean'){ //typeof(obj) === 'string'){
@@ -52,7 +15,6 @@ function stringifyJSON(input) {
     if (input.getMonth){
         return `"${input.toISOString()}"`;
     }
-
     // Test for Arrays
     if(Array.isArray(input) && input.length === 0) {
         return '[]';
@@ -65,9 +27,7 @@ function stringifyJSON(input) {
         //     tempArray.push(item)
         // }
         return "[" + tempArray.toString() + "]"
-
     }
-
     // Test for Objects
     //if (input.constructor.name == "Object"){
         let tempArrayOfKeysValues = []
@@ -90,28 +50,12 @@ function stringifyJSON(input) {
             } else if (typeof ValuesToStrinfigy === 'boolean' || typeof ValuesToStrinfigy === 'number' || ValuesToStrinfigy === null) {
                 tempArrayOfKeysValues.push(keysToStringify + ValuesToStrinfigy);
             
-            // } else if (key instanceof Date) {
-            //     tempArrayOfKeysValues.push("\"" + key.toJSON() + "\"");
-                //return;
-          
-            // } else if (Array.isArray(ValuesToStrinfigy)){
-            //     let tempArray =[];
-            //     ValuesToStrinfigy.forEach(item => tempArray.push(stringifyJSON(item)))
-            
             } else if (ValuesToStrinfigy instanceof Object) {
                 tempArrayOfKeysValues.push(keysToStringify + stringifyJSON(ValuesToStrinfigy));
-            
             }
           });
             return "{" + tempArrayOfKeysValues + "}"
         }
     } 
 
-// console.log(typeof(stringifyJSON(testString)))
-// console.log(stringifyJSON(testString))
-
-// console.log(JSON.stringify(testString))
-// console.log(typeof(JSON.stringify(testString)))
-
-//   let result = stringifiableValues.toString();
-//   console.log(result)
+    stringifyJSON(stringifiableValues);
