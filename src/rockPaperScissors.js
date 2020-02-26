ed23d29dab1585f07d39f2a2d61b20a3b11b2f15
Saggitarie <saggitarie@gmail.com>
2020-02-26 16:10:03 +0900

    Wrote RockPaperScissors method

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,13 +1,35 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
+const rockPaperScissors = (rounds = 3) => {
   // Declare variable for final output and weapons
+  let allPatterns = [];
+  let weapons = ["rock", "paper", "scissors"];
 
+  const playGame = (rounds, result = []) => {
   // Write Base Case here
     // When number of rounds is less than 0
 
+    if(rounds < 1){
+      allPatterns.push(result);
+      return;
+    }
+
   // Write Recusive Case here
     // 4 round for all three patterns: rock, paper and scissors
+    for(let i = 0; i< weapons.length; i++){
+      const weapon = weapons[i];
+      playGame(rounds - 1, result.concat(weapon));
+    }
+  }
 
-  return;
+  playGame(rounds);
+
+  console.log(allPatterns);
+
+  // allPatterns.forEach(el => {
+  //   tmp = el.filter(weapon => weapon !== undefined)
+  // });
+
+  // console.log(tmp);
+  return allPatterns;
 };
