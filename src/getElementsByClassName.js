205a4554809631220d63a41aa5d8e60daa155b9b
Saggitarie <saggitarie@gmail.com>
2020-02-28 11:58:45 +0900

    getElementsById method implemented

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,25 +5,41 @@ const getElementsByClassName = target => {
 
   const getElements = DOMstring => {
 
-    console.log(DOMstring);
+    // console.log(DOMstring);
 
     // Base case
-    if(DOMstring.lastChild === null){
-      return;
+    // if(DOMstring.childNodes.length === 0 && DOMstring.className.includes(target)) {
+    //   targetNodes.push(DOMstring);
+    //   return;
+    // }
+
+    if(DOMstring.childNodes.length === 0) {
+      // let textNode = DOMstring.textContent;
+      // console.log(textNode);
+      if(DOMstring.textContent !== ""){
+        return;
+      }else if(DOMstring.className.includes(target)){
+        targetNodes.push(DOMstring);
+        return;
+      }else {
+        return;
+      }
     }
 
     // Recursive case
     if(DOMstring.className.includes(target)){
       targetNodes.push(DOMstring);
-
     }
+
     let node = DOMstring.lastChild;
-    getElements(node);
+
+    if(node !== null) getElements(node);
   }
 
   getElements(document.body);
 
   console.log(targetNodes);
   console.log("--------------------");
+
   return targetNodes;
 };
