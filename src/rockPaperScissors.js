c48186366c4a17393cf4f519f307b18f897d1d31
cwcraw <cwcraw@gmail.com>
2020-03-01 07:30:05 +0900

    Need to commit more often. This is passing.

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,19 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
-};
+const rockPaperScissors = (rounds = 3) => {
+  const output = []
+  const hands = ['rock', 'paper', 'scissors']
+  const play = (rounds, result = []) => {
+    console.log(rounds)
+    if (rounds === 0) {
+      console.log('ending round')
+      output.push(result)
+      return
+    }
+    for (const el of hands) {
+      play(rounds - 1, result.concat(el))
+    }
+  }
+  play(rounds)
+  return output
+}
