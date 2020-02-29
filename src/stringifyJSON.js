db02910bcdc999d6a26939f37c9d5ea7a5e55534
Saggitarie <saggitarie@gmail.com>
2020-02-29 18:02:13 +0900

    11 more tests to go

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,56 +3,72 @@
 const stringifyJSON = (input) => {
   let result = "";
   let dataType = "";
+  let counter = 0;
+  let valLength = 0;
+  let isObject = false;
+  let isArray = false;
 
   let isPrimitive = ["[object Number]", "[object Boolean]", "[object Null]"];
   //, "[object String]"
   // console.log(input);
 
   const covertToString = (val) => {
-    let counter = 0;
     let dataType = Object.prototype.toString.call(val);
-    let valLength = 0;
 
-    console.log("Type of Data: " + typeof val);
+
+    // console.log("Type of Data: " + typeof val);
 
     if(dataType === "[object Array]") {
       valLength = val.length;
-      console.log("Array length: " + valLength);
+      isArray = true;
+      // console.log("Array length: " + valLength);
     }
 
     if(dataType === "[object Object]") {
       valLength = Object.keys(val).length;
-      console.log("Object length: " + valLength);
+      isObject = true;
+      // console.log("Object length: " + valLength);
     }
 
     // Base case
     if(isPrimitive.includes(dataType)){
-      result = `${val}`;
+      result += `${val}`;
       return;
     }else if(dataType === "[object String]") {
-      result =  `"${val}"`;
+      result +=  `"${val}"`;
       return;
     }else {
       // Base case for object and arrays
-
-      // When the pointer is at the end of the array
+      if(isObject && valLength === 0){
+          result += `{}`;
+          return;
+      } else if(isArray && valLength === 0){
+          result += `[]`;
+          return;
+      }
     }
 
     //Recursive case
     for(el in val){
       counter = counter + 1;
-
-      // if(counter === valLength - 1) `"${val[el]}`
-
       covertToString(val[el]);
     }
-
   }
+
+
 
   covertToString(input);
 
-  console.log("result "+ result);
+  if(isObject && valLength !== 0){
+    result = `{${result}}`;
+  }
+
+  if(isArray && valLength !== 0){
+    result = `[${result}]`;
+  }
+
   return result;
+
 };
 
   // Base case
