e0fafe03ceba979bfb69d94ca14484ad7e5f1111
Zowie <minjiayi22@gmail.com>
2020-02-29 13:27:21 +0900

    Passed Jasmine

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,19 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (rounds) => {
+  let out = [];
+  const weapons = ["rock", "scissors", "paper"];
+  const play = (rounds, result = []) => {
+    if (rounds < 1) {
+      out.push(result);
+      return out;
+    }
+    weapons.map((weapon) => play(rounds - 1, result.concat(weapon)));
+  };
+  if (rounds == undefined) {
+    play(3);
+  } else {
+    play(rounds);
+  }
+  return out;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,30 @@
 /* exported stringifyJSON */
-
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (value) => {
+  if (typeof value == "string") {
+    return `"${value}"`;
+  } else if (typeof value == "number" || typeof value == "boolean") {
+    return value.toString();
+  } else if (value == null) {
+    return "null";
+  } else if (typeof value == "object" && !Array.isArray(value)) {
+    let objString = [];
+    for (const key in value) {
+      if (typeof value[key] == "function" || value[key] === undefined) {
+        continue;
+      } else {
+        objString.push(`${stringifyJSON(key)}:${stringifyJSON(value[key])}`);
+      }
+    }
+    return `{${objString.join(",")}}`;
+  } else if (Array.isArray(value)) {
+    let arrayString = [];
+    for (const i of value) {
+      if (i === undefined || typeof i == "function") {
+        continue;
+      } else {
+        arrayString.push(stringifyJSON(i));
+      }
+    }
+    return `[${arrayString.join(",")}]`;
+  }
 };
