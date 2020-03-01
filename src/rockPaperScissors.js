0a638d7d3c0535e5be2fba943206bb9704961d61
cwcraw <cwcraw@gmail.com>
2020-03-01 16:21:25 +0900

    Removed Console.logs

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -4,9 +4,9 @@ const rockPaperScissors = (rounds = 3) => {
   const output = []
   const hands = ['rock', 'paper', 'scissors']
   const play = (rounds, result = []) => {
-    console.log(rounds)
+    
     if (rounds === 0) {
-      console.log('ending round')
+    
       output.push(result)
       return
     }
