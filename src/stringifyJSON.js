062aedd3ac518ba520e1499d889d640c08e62bb8
Zowie <minjiayi22@gmail.com>
2020-03-01 14:59:18 +0900

    Fixed the new Date case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,4 +1,34 @@
 /* exported stringifyJSON */
+// const stringifyJSON = (value) => {
+//   if (typeof value == "string") {
+//     return `"${value}"`;
+//   } else if (typeof value == "number" || typeof value == "boolean") {
+//     return value.toString();
+//   } else if (value == null) {
+//     return "null";
+//   } else if (typeof value == "object" && !Array.isArray(value)) {
+//     let objString = [];
+//     for (const key in value) {
+//       if (typeof value[key] == "function" || value[key] === undefined) {
+//         continue;
+//       } else {
+//         objString.push(`${stringifyJSON(key)}:${stringifyJSON(value[key])}`);
+//       }
+//     }
+//     return `{${objString.join(",")}}`;
+//   } else if (Array.isArray(value)) {
+//     let arrayString = [];
+//     for (const i of value) {
+//       if (typeof i === undefined || typeof i == "function") {
+//         continue;
+//       } else {
+//         arrayString.push(stringifyJSON(i));
+//       }
+//     }
+//     return `[${arrayString.join(",")}]`;
+//   }
+// };
+
 const stringifyJSON = (value) => {
   if (typeof value == "string") {
     return `"${value}"`;
@@ -6,25 +36,31 @@ const stringifyJSON = (value) => {
     return value.toString();
   } else if (value == null) {
     return "null";
-  } else if (typeof value == "object" && !Array.isArray(value)) {
-    let objString = [];
-    for (const key in value) {
-      if (typeof value[key] == "function" || value[key] === undefined) {
-        continue;
-      } else {
-        objString.push(`${stringifyJSON(key)}:${stringifyJSON(value[key])}`);
+  } else if (typeof value == "object") {
+    let toString = Object.prototype.toString;
+    if (toString.call(value) == "[object Date]") {
+      console.log(value);
+      return `"${value.toISOString()}"`;
+    } else if (Array.isArray(value)) {
+      let arrayString = [];
+      for (const i of value) {
+        if (typeof i === undefined || typeof i == "function") {
+          continue;
+        } else {
+          arrayString.push(stringifyJSON(i));
+        }
       }
-    }
-    return `{${objString.join(",")}}`;
-  } else if (Array.isArray(value)) {
-    let arrayString = [];
-    for (const i of value) {
-      if (i === undefined || typeof i == "function") {
-        continue;
-      } else {
-        arrayString.push(stringifyJSON(i));
+      return `[${arrayString.join(",")}]`;
+    } else if (toString.call(value) == "[object Object]") {
+      let objString = [];
+      for (const key in value) {
+        if (typeof value[key] == "function" || value[key] === undefined) {
+          continue;
+        } else {
+          objString.push(`${stringifyJSON(key)}:${stringifyJSON(value[key])}`);
+        }
       }
+      return `{${objString.join(",")}}`;
     }
-    return `[${arrayString.join(",")}]`;
   }
 };
