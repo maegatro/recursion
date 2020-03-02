c70bb97830e7f0660f8ba4c06f02de85ff641b7f
Roger V <rogerroger.v@gmail.com>
2020-03-03 00:29:59 +0900

    Add a rough solution *non-working* to visualize the concept

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,7 +1,18 @@
 /* exported rockPaperScissors */
 
 const rockPaperScissors = (rounds = 3) => {
+  const hands = ["rock", "paper", "scissors"]
   let output = []
 
+  function game(rounds, handsPlayed){
+  if (rounds < 1) {
+    output.push(handsPlayed)
+    return output
+  } else
+    for (let i = 0; i < hands.length; i++) {
+      let currentHand = hands[i]
+      game(rounds - 1, handsPlayed.push(currentHand))
+    }
+  }
   return output
 };
