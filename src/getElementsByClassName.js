4cad79830f254515dd161623daf18a6bd7e51f36
maegatro <shuntaromaekawa@gmail.com>
2020-03-02 19:16:05 +0900

    Wrote stringfyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,18 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
+const getElementsByClassName = (className) => {
   // YOUR CODE HERE
+  let result = [];
+  let searchElements = (elements) => {
+    if(elements.classList && elements.classList.contains(className)){
+      result.push(elements);
+    };
+    if(elements.childNodes){
+      elements.childNodes.forEach(e => {
+        searchElements(e);
+      })
+    }
+  };
+  searchElements(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,22 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
+const stringifyJSON = (value) => {
   // YOUR CODE HERE
+  if(typeof value === 'number' || typeof value === 'boolean' || value === null){
+    return `${value}`;
+  } else if(typeof value === 'string'){
+    return `"${value}"`
+  } else if(Array.isArray(value)){
+    let result = value.map(e => stringifyJSON(e));
+    return "[" + result.join() + "]";
+  } else if (typeof value === 'object'){
+    let result = [];
+    for(let key in value){
+      let resultValue = stringifyJSON(value[key]);
+      if(typeof resultValue !== "undefined"){
+        result.push(`"${key}":${stringifyJSON(value[key])}`);
+      }
+    }
+    return "{" + result.join() + "}"; 
+  }
 };
