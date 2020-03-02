8e6a6e15a82d084f6caa0d2f57e043aabb002177
Kapakahi <rubbishman@gmail.com>
2020-03-02 19:33:31 +0900

    Completed  DOM assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,19 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  let result = [];
+  function traverse (node) {
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+    if (node.childNodes.length > 0) {
+      for (let element of node.childNodes) {
+        traverse (element);
+      }    
+    }
+  }
+  traverse(document.body);
+  return result
 };
+
+
