2ae7a6afbdc08803e77f5e945cb1ed7a73012638
Riku Kawano <rrrikukawano@gmail.com>
2020-03-03 22:02:48 +0900

    Wrote getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,21 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClass) => {
+  const result = [];
+  
+  function findTarget(element, collection) {
+    const childrenCount = element.children.length;
+    const children = element.children;
+  
+    if (element.classList.contains(targetClass)) {
+      result.push(element);
+    }
+    if (childrenCount === 0) return;
+    for (let i = 0; i < childrenCount; i++){
+      findTarget(children[i], collection.concat(children[i]));
+    }
+  }
+
+  findTarget(document.body, [])
+  return result;
 };
