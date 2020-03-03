fdd0c922e1527692d897ee28c292dbc48c4fccc9
1st <eric.e.nicolas@gmail.com>
2020-03-03 15:06:32 +0800

    array stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,30 @@
 /* exported stringifyJSON */
+obj1 =[1,2,3,[2,3],4];
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (obj) => {
+  var result=[];
+  console.log(!Array.isArray(obj));
+  console.log(typeof obj !== 'object');
+  console.log(obj !== null);
+  if ((!Array.isArray(obj) || typeof obj !== 'object') && obj !== null){
+    console.log('enter');
+    return ''+obj;
+  }
+  if(obj == null){
+    return ''+'null';
+  }
+  if(typeof obj =='function'){
+    return ''+obj;
+  }
+  if(Array.isArray(obj)){
+    var arr2=[];
+    for (var i = 0; i < obj.length; i++){
+      arr2.push(stringifyJSON(obj[i]));
+    }
+    var arr3 = '['+arr2.join(',')+']';
+    result.push(arr3);
+  }
+  
+  return '{'+result.join(',')+'}';
 };
+console.log(stringifyJSON(obj1));
---
+++ src/test.js
@@ -0,0 +1,7 @@
+//var arr=[1,2,3,4];
+//var arr2=[];
+//for(var i = 0; i < arr.length;i++){
+//    arr2.push(''+arr[i]);
+//}
+//var arr3='['+arr2.join(',')+']';
+//console.log(arr3);
