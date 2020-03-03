1f39bb38c4ccf8bcb13b06936baab0d691f31d8c
Roger V <rogerroger.v@gmail.com>
2020-03-03 12:21:18 +0900

    Add the functioning rockPaperScissors function

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -4,15 +4,16 @@ const rockPaperScissors = (rounds = 3) => {
   const hands = ["rock", "paper", "scissors"]
   let output = []
 
-  function game(rounds, handsPlayed){
+  function game(rounds, handsPlayed = []){
   if (rounds < 1) {
     output.push(handsPlayed)
     return output
   } else
     for (let i = 0; i < hands.length; i++) {
-      let currentHand = hands[i]
-      game(rounds - 1, handsPlayed.push(currentHand))
+      let nowHand = hands[i]
+      game(rounds - 1, handsPlayed.concat(nowHand))
     }
   }
+  game(rounds)
   return output
 };
