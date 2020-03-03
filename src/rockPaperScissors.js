6699ac5b2221769dc9fbf1da6949278bc6ad5ef3
Jeff Bentley <jbentley449@gmail.com>
2020-03-03 15:41:27 +0900

    first commit, completion of rock paper scissors

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,21 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (rounds = 3) => {
+  const potentialCombinations = [];
+  const choices = ["rock", "paper", "scissors"];
+
+  const game = (roundsLeft, choicesMade) => {
+    if (roundsLeft === 0){
+      potentialCombinations.push(choicesMade);
+      return;
+    } else {
+      for (let i = 0; i < choices.length; i++){
+        let choice = choices[i];
+         game(roundsLeft - 1, choicesMade.concat(choice));
+      }
+    }
+  };
+  game(rounds, []);
+  return potentialCombinations;
 };
+
