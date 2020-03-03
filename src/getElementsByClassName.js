70837fefb7665e9f850b2c69e53a296fd11837e4
Jeff Bentley <jbentley449@gmail.com>
2020-03-04 01:38:34 +0900

    Completed the getElementsByClassName problem

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,16 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (name) => {
+  const output = [];
+
+  const searchForClassName = (elements) => {
+    if (elements.children === undefined) return;
+    if (elements.classList && elements.classList.contains(name)) output.push(elements);
+
+    for (let element of elements.children){
+      searchForClassName(element);
+    };
+  }
+  searchForClassName(document);
+  return output;
 };
