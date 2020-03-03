2e0fa22f92c5ef0a1745fe96e096cee707c83b1a
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-03-03 21:45:28 +0900

    First commit, getsElementsByClassName.js & rockPaperScissors.js added

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,26 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  
+  elementsWithClassName = [];
+
+  function findElementsWithClassNames (node) {
+    
+    if(node.classList !== undefined && node.classList.contains(className)) {
+      elementsWithClassName.push(node);
+    }
+
+    if(node.childNodes.length > 0) {   // recursion case
+      for(let prop of node.childNodes) {
+        findElementsWithClassNames(prop);
+      }
+    }
+    
+    // case exits when it reaches the end of the stack
+  }
+
+  findElementsWithClassNames(document.body);
+
+  return elementsWithClassName;
+
 };
--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,26 @@
 /* exported rockPaperScissors */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (rounds) => {
+  
+  if(rounds == undefined) {
+    rounds = 3;
+  }
+
+  let weapons = ["rock", "paper", "scissors"];
+  let output = [];
+  
+  function play (rounds, result = []) {
+    if (rounds < 1) {
+      output.push(result);
+      return;
+    }
+
+    for (let i = 0; i < weapons.length; i++) {
+      let weapon = weapons[i];
+      play(rounds - 1, result.concat(weapon));
+    }
+  }
+
+  play(rounds);
+  return output;
 };
