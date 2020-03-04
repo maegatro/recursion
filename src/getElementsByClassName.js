31a56f8ad2c14d49dcee15e0cf9c1ec705febb8a
Yuya Harada <tzou11291996@gmail.com>
2020-03-04 17:09:31 +0900

    first commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,28 @@
-/* exported getElementsByClassName */
-
-const getElementsByClassName = () => {
+const getElementsByClassName = (target) => {
   // YOUR CODE HERE
+  let result = [];
+  const recurse = (initialNode) => {
+      //if it's classList contains className
+      // console.log(initialNode);
+
+    if (initialNode.classList !== undefined && initialNode.classList.contains(target)){
+      result.push(initialNode);
+    } 
+    for (let i = 0; i < initialNode.childNodes.length; i++){
+      let node = initialNode.childNodes[i];
+      // console.log("node", node);
+      recurse(node);
+    }
+  };
+  recurse(document.body);
+  return result;
 };
+
+
+
+
+
+      
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,47 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
+const stringifyJSON = (info) => {
   // YOUR CODE HERE
+  // console.log(typeof info);
+  let result = "";
+
+  const recurse = (value) => {
+    console.log("aaa", value);
+    if (typeof value === "string"){
+      result = `"${value}"`;
+    } else if (value === null){
+      result = "null"; 
+
+    } else if (Array.isArray(value)){
+      console.log("array", value);
+        if (value.length < 1){
+        result = `[${value}]`;
+        } else {
+          result = "["
+          for (let i = 0; i < value.length; i++){
+            if (typeof value[i] === "string"){
+              result += '"' + value[i] + '"' + ",";
+                if (i === value.length - 1){
+                  result = result.slice(0, -1) + "]";
+              } 
+            } 
+            else if(typeof value[i] == "number"){
+              result += value[i] + ",";
+                if (i === value.length - 1){
+                  result = result.slice(0, -1) + "]";
+                }
+            } else {
+              result = "'" + value + "'";
+            }
+          }
+        }
+    } else {
+      result =  value.toString();
+    }
+  
 };
+  recurse(info);
+  return result; 
+
+ 
+}
