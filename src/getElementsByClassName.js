08d247ff2eb57a3afbe8c2c412ee47fe64e9481a
Atila Lima <atilamanager@gmail.com>
2020-03-05 01:04:49 +0900

    Adding getElementsByClasName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,22 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  let elementWithClassName = [];
+
+  function findElementWithClassName(element) {
+    if (
+      element.classList !== undefined &&
+      element.classList.contains(className)
+    ) {
+      elementWithClassName.push(element);
+    }
+    if (element.childElement.length > 0) {
+      for (let prop of element.childElement) {
+        findElementWithClassName(prop);
+      }
+    }
+  }
+  findElementWithClassName(document.body);
+
+  return elementWithClassName;
 };
