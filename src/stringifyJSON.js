a113a8b8f0c3db738d5d22cf67999c656ec50dda
Julia Gross <sakurtoshi@gmail.com>
2020-03-04 22:46:57 +0900

    still working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,15 +1,28 @@
 /* exported stringifyJSON */
 
 const stringifyJSON = (value) => {
-  let result = "";
+  
   //booleans, numbers strings
   if (typeof value === 'string'){
     return `"${value}"`;
   } else if (typeof value === 'boolean' || typeof value === 'number'){
     return `${value}`;
   } 
+  if (value === null){
+    return "null";
+  }
+  if (Array.isArray(value)){
+    let inputArray = [];
+    if (value.length === 0){
+      return '[]';
+    } else {
+      for (const i of value){
+        inputArray.push(stringifyJSON(i));
+      }
+    }
+    return "[" + inputArray.toString() + "]";
+  }
   //then arrays and objects
   //how to go through arrays and objects
   stringifyJSON(value);
-  return result;
 };
