1575769edfc7aa49957b1aa036f230b749980d08
Roger V <rogerroger.v@gmail.com>
2020-03-04 11:04:09 +0900

    Add semi working object case -still tweaking approach

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,24 +8,39 @@ const stringifyJSON = (objectToStringify) => {
     return (`"` + str + `"`)
   }
   
-  function stringing(input){
-    let result
-
-    if (input === null)
-      result = `null`
+  function stringing(input, index){
+    let result, tmpBuilder
+    index = index || 0
 
     switch (typeof(input)){
       case `number`:
       case `boolean`:
         result = `${input}`
         break;
+
       case `string`:
         result = stringString(input)
         break;
+
       case `undefined`:
       case `function`:
-        result = "{}"
+        result = `{}`
         break;
+
+      case `object`:
+        if (input === null){
+        result = `null`
+        } else if (Array.isArray(input)){
+          if (index = input.length){
+            (result = `[]`)
+          } else
+            for (let i = 0; i < input.length; i++) {
+              tmpBuilder = tmpBuilder + `'` + stringing(input[i])
+          }
+          result = (`[` + tmpBuilder + `]`)
+        } else
+        break;
+
       default:
         break;
     }
