c42cd04968653df3896d2d2ddb7fd219cd044bad
Julia Gross <sakurtoshi@gmail.com>
2020-03-04 15:21:19 +0900

    got getElements done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,18 @@
 /* exported getElementsByClassName */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  let result = [];
+  let node = document.body;
+  //exit case here
+  const checkForClass = (node) => {
+    if (node.classList && node.classList.contains(className)){
+      result.push(node);
+    }
+    //recursive case here
+    for (let i = 0; i < node.childNodes.length; i++){
+      checkForClass(node.childNodes[i]);
+    }
+  }
+  checkForClass(node);
+  return result;
 };
