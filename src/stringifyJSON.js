1350d0e1a82a5a70176e0b5556e66cd0e2037a7f
Julia Gross <sakurtoshi@gmail.com>
2020-03-04 21:30:11 +0900

    got the easiest part of this done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,15 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (value) => {
+  let result = "";
+  //booleans, numbers strings
+  if (typeof value === 'string'){
+    return `"${value}"`;
+  } else if (typeof value === 'boolean' || typeof value === 'number'){
+    return `${value}`;
+  } 
+  //then arrays and objects
+  //how to go through arrays and objects
+  stringifyJSON(value);
+  return result;
 };
