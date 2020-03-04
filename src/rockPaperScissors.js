278f0d4282f704af44094e65ee53b73906c946c5
Atila Lima <atilamanager@gmail.com>
2020-03-05 01:05:55 +0900

    Adding rockPaperScissors.js

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,5 +1,24 @@
 /* exported rockPaperScissors */
 
 const rockPaperScissors = () => {
-  // Your code here
+  if (rounds === undefined) {
+    rounds = 3;
+  }
+  let weapons = ["rock", "paper", "scissors"];
+  let outcome = [];
+
+  const play = (rounds, result = []) => {
+    // exit case
+    if (rounds < 1) {
+      outcome.push(result);
+      return;
+    }
+    // recursive case
+    for (let i = 0; i < weapons.length; i++) {
+      const weapon = weapons[i];
+      play(rounds - 1, result.concat(weapon));
+    }
+  };
+  play(rounds);
+  return outcome;
 };
