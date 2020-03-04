92f90f441eecac224eeca87a4de403d47b03d233
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-03-04 12:15:03 +0900

    Finished all assignments!

--- spec/stringifyJSON.js
+++ spec/stringifyJSON.js
@@ -1,25 +1,25 @@
 const stringifiableValues = [
-  100000,
-  true,
-  false,
-  null,
-  "Code Chrysalis",
-  [],
-  [10000],
-  ["yan"],
-  [10000, "kani"],
-  [1, 2, 1000, 0, -1, -3, -3.14, 3.14, 0.1099999, 1000.9999],
-  [[[["rachel"]]]],
-  [10000, [[1, 2, 3, 4], 5, 8]],
-  {},
-  { a: "is for apple" },
-  { foo: true, bar: false, baz: null },
-  { "boolean, true": true, "boolean, false": false, null: null },
+  100000, // number
+  true, // boolean
+  false, // boolean
+  null, // boolean
+  "Code Chrysalis", // string
+  [], // array
+  [10000], // array with number
+  ["yan"], // array with string
+  [10000, "kani"], // array with number and string
+  [1, 2, 1000, 0, -1, -3, -3.14, 3.14, 0.1099999, 1000.9999], // many numbers
+  [[[["rachel"]]]], // deeply nested array within array with string value
+  [10000, [[1, 2, 3, 4], 5, 8]], // numbers and some string values
+  {}, // object
+  { a: "is for apple" }, // object with element title plus string
+  { foo: true, bar: false, baz: null }, // object with element titles plus booleans
+  { "boolean, true": true, "boolean, false": false, null: null }, // object with some titles and boolean values
   // basic nesting
-  { a: { b: "c" } },
-  { a: ["b", "c"] },
-  [{ a: "b" }, { c: "d" }],
-  { a: [], c: {}, b: true }
+  { a: { b: "c" } }, // deep nested object
+  { a: ["b", "c"] }, // deep nested object with array
+  [{ a: "b" }, { c: "d" }], // array of objects
+  { a: [], c: {}, b: true } // object with array and object and boolean
 ];
 
 const unstringifiableValues = [
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,33 @@
 /* exported stringifyJSON */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (value) => {
+
+  if(typeof value === "number" || typeof value === "boolean" || value === null) {
+    return `${value}`;
+  } 
+  
+  if(typeof value === "string") {      
+    return `"${value}"`;
+  } 
+  
+  if (Array.isArray(value)) {
+    let result = [];
+    value.map(function (item) {
+      result.push(stringifyJSON(item));
+    });
+    return "[" + result.join() + "]";
+  } 
+  
+  if (typeof value === "object") {
+    let result = [];
+    for(let key in value) {
+      let dataType = value[key];
+      let stringifyValue = stringifyJSON(dataType);
+
+      if(typeof stringifyValue !== "undefined") {
+        result.push(`"${key}":${stringifyValue}`);
+      }
+    }
+  return "{" + result.join(',') + "}";
+  }
 };
